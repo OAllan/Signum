2081ce89eba078e4fba0c814f991bc24472189b3
oscarruiz <oscar.ruiz@konfio.mx>
2017-09-27 16:35:13 -0500

    Core logic added

--- src/main/java/com/itesm/signum/Signum.java
+++ src/main/java/com/itesm/signum/Signum.java
@@ -5,12 +5,41 @@
  */
 package com.itesm.signum;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.net.URI;
+import javax.swing.ImageIcon;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.FileEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
 /**
  *
  * @author allanruiz
  */
 public class Signum {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Signum.setup();
+        Signum.start();
         
+        
+    }
+    
+    public static void setup(){
+        File tempDir = new File("temp");
+        tempDir.mkdir();
+        tempDir.deleteOnExit();
+    }
+    
+    public static void start(){
+        VideoProcessor video = new VideoProcessor(null, null);
+        video.loadVideo("");
+        video.startAnalysis();
     }
 }
